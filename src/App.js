5a34bf85ce707698038acd025ac17f4d2ea8e264
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-10-27 15:08:43 -0700

    adjust code per platform

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import './App.css';
 
 import PersonCard from './Components/PersonCard';
 
+var peoples = [
+  {"firstName":"Ms.", "lastName":"Piggy", "age":47, "hairColor":"Blonde"},
+  {"firstName":"Kermit The", "lastName":"Frog", "age":55, "hairColor":"Green"},
+  {"firstName":"Fozzy", "lastName":"Bear", "age":42, "hairColor":"Brown"},
+  {"firstName":"Animal", "lastName":"Anima", "age":73, "hairColor":"Orange"},
+]
+
 function App() {
   
   return (
     <div className="App">
-      <PersonCard  firstName = {"Ms."} lastName = {"Piggy"} age = {47} hairColor = {"Blonde"} />
-
-      <PersonCard  firstName = {"Kermit The"} lastName = {"Frog"} age = {55} hairColor = {"Green"} />
-
-      <PersonCard  firstName = {"Fozzy"} lastName = {"Bear"} age = {42} hairColor = {"Brown"} />
-
-      <PersonCard  firstName = {"Anima"} lastName = {"Animal"} age = {73} hairColor = {"Orange"} />
+      {peoples.map(person => {
+        return <PersonCard firstName = {person.firstName} lastName = {person.lastName} age = {person.age} hairColor={person.hairColor} />
+      })
+      }
     </div>
   );
 }
--- src/Components/PersonCard.js
+++ src/Components/PersonCard.js
@@ -2,12 +2,12 @@ import React, {Component} from "react";
 
 class MyNewComponent extends Component{
     render() {
-        const { firstName, lastName, age, hairColor} = this.props;
+        
         return (
             <div>
                 <h3> { this.props.lastName }, { this.props.firstName }</h3>
-                <h6>Age: { age }</h6>
-                <h6>Hair Color: { hairColor }</h6>
+                <h6>Age: { this.props.age }</h6>
+                <h6>Hair Color: { this.props.hairColor }</h6>
             </div>
         )
     }
